b31ae3c9e459029f68a22db84315c983b708ff99
さちん <kohaku332@hotmail.com>
2015-09-10 07:39:59 +0900

    2015/09/10 rev1.0

--- ClalisSample/Activity/SubActivityJson41/frmClalisEmotionJson41.Designer.cs
+++ ClalisSample/Activity/SubActivityJson41/frmClalisEmotionJson41.Designer.cs
@@ -28,9 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grpInput.SuspendLayout();
+            this.grpResult.SuspendLayout();
+            this.grpCtl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spl)).BeginInit();
+            this.spl.Panel1.SuspendLayout();
+            this.spl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+
+            // 
+            // spl
+            // 
+            // 
+            // frmClalisEmotionJson41
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Name = "frmClalisEmotionJson41";
             this.Text = "frmClalisEmotionJson41";
+            this.grpInput.ResumeLayout(false);
+            this.grpInput.PerformLayout();
+            this.grpResult.ResumeLayout(false);
+            this.grpCtl.ResumeLayout(false);
+            this.spl.Panel1.ResumeLayout(false);
+            this.spl.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spl)).EndInit();
+            this.spl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
